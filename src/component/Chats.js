9fb141eb66e64366068c7395be43c98473d8d875
isaac <oseasisaac91@gmail.com>
2019-06-05 11:14:28 -0400

    recieves messages and can do post requests with messages. NewChatHandler is modified to avoid the same chat being sent twice, messages load and are displayed

--- src/component/Chats.js
+++ src/component/Chats.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 
+
 const Chats = props => (
   <ul>
-    {props.chatIds.map(chatId => <li key={chatId}>{chatId}</li>)}
+    {props.chatIds.map(chatId => <li onClick={() => props.selectChat(chatId)} key={chatId}>
+      {chatId}
+    </li>)}
   </ul>
 )
 
--- src/component/MessagingDisplay.js
+++ src/component/MessagingDisplay.js
@@ -10,7 +10,7 @@ class MessagingDisplay extends Component {
     user:  null,
     chatIds: [],
     messages: [],
-    currentChat: null
+    currentChatId: null
   }
 
   createUser = () => {
@@ -44,23 +44,54 @@ class MessagingDisplay extends Component {
     .then(newChat => this.newChatHandler(newChat, 'not null'))
   }
 
+  createMessage = () => {
+    fetch('http://localhost:3090/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        chat_id: this.state.currentChatId,
+        user_id: this.state.user.id,
+        text: this.state.messageInput
+      })
+    })
+  }
+
   newChatHandler = (newChat, created = null) => {
-    if(created === 'not null'){
-      return this.setState({currentChat: newChat.chat.id, chatIds: [newChat.chat.id, ...this.state.chatIds], messages: [...newChat.chat.messages, ...this.state.messages]})
+    if(created === 'not null' || this.state.currentChatId === null){
+      this.setState({currentChatId: newChat.chat.id, chatIds: [newChat.chat.id, ...this.state.chatIds], messages: [...newChat.chat.messages, ...this.state.messages]})
+    } else {
+
+      this.setState({chatIds: [newChat.chat.id, ...this.state.chatIds], messages: [...newChat.chat.messages, ...this.state.messages]})
     }
-    return this.setState({chatIds: [newChat.chat.id, ...this.state.chatIds], messages: [...newChat.chat.messages, ...this.state.messages]})
+  }
+
+  selectChat = chatId => {
+    this.setState({currentChatId: chatId})
+  }
+
+  newMessageHandler = newMessage => {
+    this.setState({messages: [...this.state.messages, newMessage]})
   }
 
 
 
   render () {
-    console.log(this.state.currentChat)
+    console.log(this.state.currentChatId)
     return(
       <div>
         <ActionCableConsumer
           channel={{channel: 'ChatChannel', user_id: this.state.user ? this.state.user.id : null}}
           onReceived={newChat => this.newChatHandler(newChat)}
         />
+        {this.state.chatIds.map(chatId =>  <div style={{display: 'none'}} key={'chatId' + chatId}><ActionCableConsumer
+          channel={{channel: 'MessageChannel', chat_id: chatId}}
+          onReceived={newMessage => this.newMessageHandler(newMessage)}
+          />
+          </div>
+        )}
         <UsersSidebar
           users={this.props.users}
           createChat={this.createChat}
@@ -70,14 +101,15 @@ class MessagingDisplay extends Component {
             <ChatsDisplay
               chatIds={this.state.chatIds}
               messages={this.state.messages}
-              currentChat={this.state.currentChat}
+              currentChatId={this.state.currentChatId}
+              selectChat={this.selectChat}
             />
           </div>
           {this.state.user ?
             <div>
               <h3>{this.state.user.name}</h3>
-              <input type="text" />
-              <button>Send</button>
+              <input type="text" onChange={e => this.setState({messageInput: e.target.value})}/>
+              <button onClick={() => this.createMessage()}>Send</button>
             </div>
             :
             <p>
